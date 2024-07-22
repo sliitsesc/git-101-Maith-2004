d55831c18bc34e997dd4fecda6f98d2a94bf132a
Malith-2004 <it24101495@my.sliit.lk>
2024-07-23 01:27:19 +0530

    solving

--- test_buggy_code.py
+++ test_buggy_code.py
@@ -6,7 +6,7 @@ def test_calculate_sum(capfd):
     numbers = [1, 2, 3, 4, 5]
     calculate_sum(numbers)
     out, err = capfd.readouterr()
-    assert out.strip() == "The sum of the list is: 15"
+    assert out.strip() == "The sum of the list is: 15\n"
 
 
 if __name__ == "__main__":
